ccb245ab5bbadd9d2420f06805ec6c88f70dfab7
andileeb <a.leeb@vinitor.app>
2025-05-12 14:17:32 +0200

    Add shell script to run all benchmarks

---
+++ run_benchmarks.sh
@@ -0,0 +1,22 @@
+export PYTHONPATH=.
+
+# run construction benchmarks
+for i in {2..10}; do
+  if [ $i -lt 10 ]; then
+    track="track_0${i}.t"
+  else
+    track="track_${i}.t"
+  fi
+  python -m src.benchmark --target construction --track ${track} --depth 3
+done
+
+# run bfs benchmarks
+for i in {2..10}; do
+  if [ $i -lt 10 ]; then
+    track="track_0${i}.t"
+  else
+    track="track_${i}.t"
+  fi
+  python -m src.benchmark --target bfs --track ${track}
+done
+
